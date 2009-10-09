d79a2c4b00639aab314aad600911b204aa555aef
zhoujingyu <zhoujingyu@5e21f3c8-4814-11de-8855-c187e5f49513>
2009-10-09 14:19:29 +0000

    * [cnpack-upgrade] 增加删除旧日志的任务。

---
+++ appengine/cnpack-upgrade/cron.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Copyright 2009 CnPack Team
+#
+# Author: Zhou Jingyu (zjy@cnpack.org)
+#
+
+import os
+import cgi
+import datetime
+import cndef
+import wsgiref.handlers
+
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+
+class DelOldPage(webapp.RequestHandler):
+  
+  def get(self):
+    adate = datetime.datetime.now() - datetime.timedelta(hours = 30 * 24)
+    dset = db.GqlQuery("SELECT * "
+                       "FROM CWLogs "
+                       "WHERE date < :1 "
+                       "ORDER BY date DESC "
+                       "LIMIT 0, 100", adate) 
+    if dset:
+      for rec in dset:
+        rec.delete()
+      self.response.out.write('''
+<head>
+<meta http-equiv="refresh" content="1;url=/tasks/delold/">
+</head>
+<body>''')
+      self.response.out.write('deleting old data...')
+    else:
+      self.response.out.write('delete nothing')    
+
+class CronDelOldPage(webapp.RequestHandler):
+  
+  def get(self):
+    adate = datetime.datetime.now() - datetime.timedelta(hours = 30 * 24)
+    dset = db.GqlQuery("SELECT * "
+                       "FROM CWLogs "
+                       "WHERE date < :1 "
+                       "ORDER BY date DESC "
+                       "LIMIT 0, 100", adate) 
+    if dset:
+      for rec in dset:
+        rec.delete()
+
+def main():
+  application = webapp.WSGIApplication([
+    ('/tasks/delold/', DelOldPage),
+    ('/tasks/crondelold/', CronDelOldPage)
+], debug=True)
+
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+  main()
